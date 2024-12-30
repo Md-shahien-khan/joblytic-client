a919a14d815ce818df5242ab1c7be9bf4c0ae115
Md Shahien Khan <shawonkn58@gmail.com>
2024-12-30 22:27:56 +0000

    Added sorting

--- src/pages/AllJob/AllJob.jsx
+++ src/pages/AllJob/AllJob.jsx
@@ -5,17 +5,26 @@ import RecentJobsCard from '../Home/RecentJobsCard';
 const AllJob = () => {
     const [jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [sort, setSort] = useState(false);
 
+
+    console.log(sort)
     useEffect(() =>{
-        axios.get('http://localhost:5000/jobs')
+        axios.get(`http://localhost:5000/jobs?sort=${sort}`)
         .then(res => {
             setLoading(false)
             setJobs(res.data)
         })
-    }, [])
+    }, [sort])
     return (
         <div>
             <h2 className='py-5 text-4xl -font-bold text-center'>Jobs Length : {jobs.length}</h2>
+            <div className="w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center">
+                <button onClick={() => setSort(!sort)} 
+                className={`btn btn-neutral ${sort && "btn-success"}`}>
+                    {sort == true ? "sorted by Salary" : "Sort by Salary"}
+                </button>
+            </div>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                 {
                     jobs.map(job =><RecentJobsCard key={job._id}
--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -7,16 +7,9 @@ const MyApplications = () => {
     const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
-        // fetch(`http://localhost:5000/job-application?email=${user.email}`)
-        // .then(res => res.json())
-        // .then(data => setJobs(data))
-
         axios.get(`http://localhost:5000/job-application?email=${user.email}`, 
         {withCredentials : true})
             .then(res => console.log(setJobs(res.data)))
-
-
-
     }, [user.email])
     return (
         <div>
